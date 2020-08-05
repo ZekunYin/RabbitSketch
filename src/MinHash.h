943b734676b5155696840be8dfeb4d3d3516ac88
yzk <zekun.yin@mail.sdu.edu.cn>
2020-08-05 19:44:04 +0800

    add vectorized sketch intersection

--- src/MinHash.h
+++ src/MinHash.h
@@ -50,12 +50,13 @@ public:
     void push_back32(hash32_t hash) {hashes32.push_back(hash);}
     void push_back64(hash64_t hash) {hashes64.push_back(hash);}
     bool get64() const {return use64;}
+
+    std::vector<hash32_t> hashes32;
+    std::vector<hash64_t> hashes64;
     
 private:
     
     bool use64;
-    std::vector<hash32_t> hashes32;
-    std::vector<hash64_t> hashes64;
 };
 
 
