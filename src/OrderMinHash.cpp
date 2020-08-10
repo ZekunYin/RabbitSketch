dc6764bf7edbf4045e027f1824aa167007c3a64e
yzk <zekun.yin@mail.sdu.edu.cn>
2020-08-11 07:18:13 +0800

    add AVX2 and AVX512 omh sketch with cpudispatch

--- src/OrderMinHash.cpp
+++ src/OrderMinHash.cpp
@@ -18,7 +18,7 @@ double get_sec(){
 	gettimeofday(&tv, NULL);
 	return (double) tv.tv_sec + (double) tv.tv_usec / 1000000;
 }
-#ifdef __AVX512F__
+#if defined __AVX512F__ && defined __AVX512DQ__
 static void inspect(__m512i va)
 {
 	uint64_t a[8];
@@ -28,7 +28,44 @@ static void inspect(__m512i va)
 		cout << a[i] << " ";
 	cout << "]" << endl;
 }
+#else
+#ifdef __AVX2__
+// implement by avx2
+inline __m256i avx2_mullo_epi64(__m256i a1, __m256i b1)
+{
+    __m256i albl = _mm256_mul_epu32(a1, b1);
+
+    const int shuffle= 0xb1;
+    const int blendmask = 0x55;
+    __m256i a1shuffle = _mm256_shuffle_epi32(a1, shuffle);
+    __m256i b1shuffle = _mm256_shuffle_epi32(b1, shuffle);
+
+    __m256i ahbl = _mm256_mul_epu32(a1shuffle, b1);
+    __m256i albh = _mm256_mul_epu32(a1, b1shuffle);
+
+    ahbl = _mm256_add_epi64(ahbl, albh);//sum of albh ahbl
+    ahbl = _mm256_shuffle_epi32(ahbl, shuffle);
+
+    albh = _mm256_blend_epi32(ahbl, albl, blendmask);//res without add hi32 of albl
+    __m256i zero = _mm256_set1_epi32(0);
+    albl = _mm256_blend_epi32(albl, zero, blendmask);
+
+    albh = _mm256_add_epi32(albh, albl);
+    return albh;
+
+}
+inline void inspect_avx2(__m256i va)
+{
+	cout << "[ ";
+	for(int i = 0; i < 4; i++)
+		cout << ((uint64_t*)&va)[i] << " ";
+	cout << "]" << endl;
+}
 #endif
+
+#endif
+
+
 namespace Sketch
 {
 
@@ -172,23 +209,114 @@ static void omh_pos(const std::string& seq, unsigned k, unsigned l, unsigned m, 
 	int pend_size = (mers.size() / lanes) * lanes;
 	uint64_t hashBuffer[lanes];
 
+
+	//AVX512
+	//__m512i vweight = _mm512_set1_epi64(weight);
+
+#if defined __AVX512F__  && defined __AVX512DQ__
+	__m512i vzero   = _mm512_set1_epi64(0);
+	__m512i vconst0 = _mm512_set1_epi64(0xff51afd7ed558ccd);
+	__m512i vconst1 = _mm512_set1_epi64(0xc4ceb9fe1a85ec53);
+	__mmask8 weight_msk = l > 0 ? 0xFF : 0x00;
+#else
+#if defined __AVX2__
+	//AVX2
+	__m256i vweight = _mm256_set1_epi64x(weight);
+	__m256i vconst0 = _mm256_set1_epi64x(0xff51afd7ed558ccd);
+	__m256i vconst1 = _mm256_set1_epi64x(0xc4ceb9fe1a85ec53);
+#else
+#endif
+#endif
+
 	//main sketch loop
-	for(unsigned i = 0; i < m; ++i) {
-		//const auto seed = gen64();//prg();
-
-		std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
-		//body
+	//for(unsigned i = 0; i < m; ++i) 
 		for( int id = 0; id < pend_size; id+=lanes)
+	{
+		//std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
+		//body
+	for(unsigned i = 0; i < m; ++i) 
+		//for( int id = 0; id < pend_size; id+=lanes)
 		{
+			std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
 
+
+#if defined __AVX512F__ && __AVX512DQ__
+			//using AVX512
+			__m512i va = _mm512_loadu_si512((void *)&intHash[id + 0 * 8]);
+			__m512i vocc = _mm512_loadu_si512((void *)&occ[id + 0 * 8]);
+			__m512i vb = _mm512_mask_add_epi64(va, weight_msk, va, vocc);
+			__m512i vseed = _mm512_set1_epi64(mseed[i]);
+			va = _mm512_xor_epi64(vb, vseed);
+			__m512i vtmp = _mm512_srli_epi64(va, 33);
+			vb = _mm512_xor_epi64(va, vtmp);
+			va = _mm512_mullo_epi64(vb, vconst0);
+
+			vtmp = _mm512_srli_epi64(va, 33);
+			vb = _mm512_xor_epi64(va, vtmp);
+			va = _mm512_mullo_epi64(vb, vconst1);
+			vtmp = _mm512_srli_epi64(va, 33);
+			vb = _mm512_xor_epi64(va, vtmp);
+			_mm512_storeu_si512(hashBuffer + 0 * 8, vb);
+#else
+#if defined __AVX2__
+			//using intrinsics using avx2 and unroll2
+			//TODO: not heavily tested
+			__m256i va0, va1;
+			__m256i vb0, vb1;
+			__m256i vseed = _mm256_set1_epi64x(mseed[i]);
+
+			__m256i vinthash0 = _mm256_loadu_si256((__m256i*)(intHash + id));
+			__m256i vinthash1 = _mm256_loadu_si256((__m256i*)(intHash + id + 4));
+			__m256i vocc0     = _mm256_loadu_si256((__m256i*)(occ + id));
+			__m256i vocc1     = _mm256_loadu_si256((__m256i*)(occ + id + 4));
+			va0 = avx2_mullo_epi64(vocc0, vweight);
+			va1 = avx2_mullo_epi64(vocc1, vweight);
+			vb0 = _mm256_add_epi64(vinthash0, va0); 
+			vb1 = _mm256_add_epi64(vinthash1, va1);
+			va0 = _mm256_xor_si256(vb0, vseed); 
+			va1 = _mm256_xor_si256(vb1, vseed); 
+
+			vinthash0 = _mm256_srli_epi64(va0, 33);
+			vinthash1 = _mm256_srli_epi64(va1, 33);
+			vb0 = _mm256_xor_si256(va0, vinthash0);
+			vb1 = _mm256_xor_si256(va1, vinthash1);
+			va0 = avx2_mullo_epi64(vconst0, vb0);
+			va1 = avx2_mullo_epi64(vconst0, vb1);
+
+			vinthash0 = _mm256_srli_epi64(va0, 33);
+			vinthash1 = _mm256_srli_epi64(va1, 33);
+			vb0 = _mm256_xor_si256(va0, vinthash0);
+			vb1 = _mm256_xor_si256(va1, vinthash1);
+			va0 = avx2_mullo_epi64(vconst1, vb0);
+			va1 = avx2_mullo_epi64(vconst1, vb1);
+
+			vinthash0 = _mm256_srli_epi64(va0, 33);
+			vinthash1 = _mm256_srli_epi64(va1, 33);
+			vb0 = _mm256_xor_si256(va0, vinthash0);
+			vb1 = _mm256_xor_si256(va1, vinthash1);
+			//inspect_avx2(vb0);
+			//inspect_avx2(vb1);
+			_mm256_storeu_si256((__m256i*) hashBuffer    , vb0);
+			_mm256_storeu_si256((__m256i*) (hashBuffer + 4), vb1);
+			//cout << "avx2 results:" << endl;
+			//for(int vid = 0; vid < lanes; vid++)
+			//	cout << hashBuffer[vid] << " ";
+			//cout << endl;
+
+#else
 			for(int vid = 0; vid < lanes; ++vid)
 			{
 			uint64_t kmer_int = intHash[id + vid];
-		    kmer_int += occ[id + vid] * weight;
-			//mers[id].hash = mc::murmur3_fmix(kmer_int, seed);
+			kmer_int += occ[id + vid] * weight;
 			hashBuffer[vid] = mc::murmur3_fmix(kmer_int, mseed[i]);
 			}
-			
+			//cout << "correct results:" << endl;
+			//for(int vid = 0; vid < lanes; vid++)
+			//	cout << hashBuffer[vid] << " ";
+			//cout << endl;
+#endif 
+#endif
+			//exit(0);
 			//for(int vid = 0; vid < lanes; ++vid)
 			//{
 			//if( pqueue.size() < l || hashBuffer[vid] < pqueue.top().hash)
@@ -241,9 +369,16 @@ static void omh_pos(const std::string& seq, unsigned k, unsigned l, unsigned m, 
 			}
 
 		}
-		//tail
+	}
+	//for(unsigned i = 0; i < m; ++i) 
 		for( int id = pend_size; id < mers.size(); id++)
+	{
+		//std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
+		//tail
+	for(unsigned i = 0; i < m; ++i) 
+		//for( int id = pend_size; id < mers.size(); id++)
 		{
+			std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
 			uint64_t kmer_int = intHash[id];
 		    kmer_int += occ[id] * weight;
 			//one_mer.pos = id;
@@ -259,12 +394,14 @@ static void omh_pos(const std::string& seq, unsigned k, unsigned l, unsigned m, 
 		}
 	}
 
-	for(unsigned i = 0; i < m; ++i) {
+	for(unsigned i = 0; i < m; ++i) 
+	{
+		std::priority_queue<mer_info, std::vector<mer_info>, decltype(cmp)> & pqueue = pqueues[i];
 		lmers.clear();
-		while(!pqueues[i].empty())
+		while(!pqueue.empty())
 		{
-			lmers.push_back(pqueues[i].top());
-			pqueues[i].pop();
+			lmers.push_back(pqueue.top());
+			pqueue.pop();
 		}
 		std::sort(lmers.begin(), lmers.end(), [&](const mer_info& x, const mer_info& y) { return x.pos < y.pos; });
 		assert(lmers.size() == l);
@@ -278,6 +415,10 @@ static void omh_pos(const std::string& seq, unsigned k, unsigned l, unsigned m, 
 	}
 	t2 = get_sec();
 	std::cout << "omh main sketch time: " << t2 - t1 << std::endl;
+
+	delete[] intHash;
+	delete[] occ;
+	delete[] mseed;
 }
 
 double OrderMinHash::compare_sketches(const OSketch& sk1, const OSketch& sk2, ssize_t m, bool circular) {
